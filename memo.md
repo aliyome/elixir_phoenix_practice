09b71a9000d7aa3b315288abd6c6cdb364cb8423
aliyome <aliyome@gmail.com>
2020-01-09 16:23:06 +0900

    add npm dependencies

--- memo.md
+++ memo.md
@@ -17,3 +17,7 @@
 - クエリパラメータとパスパラメータは同一変数
   - `render(conn, "show.html", name: params["name"])`
     - `?name=hoge`と`/hoge`どちらの場合も`params["name"]`で値を取得可能
+
+## Webpack
+
+- module の use は下から上に(最後に宣言されたものが最初に)適用される
--- modest_greeter/assets/webpack.config.js
+++ modest_greeter/assets/webpack.config.js
@@ -9,15 +9,15 @@ module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
   },
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
+    path: path.resolve(__dirname, '../priv/static/js'),
   },
   module: {
     rules: [
@@ -25,17 +25,27 @@ module.exports = (env, options) => ({
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../webfonts',
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+  ],
 });
