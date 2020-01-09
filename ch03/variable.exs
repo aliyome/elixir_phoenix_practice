ef2cb4d6426f4c533ebc965d786fe9f1be68f276
aliyome <aliyome@gmail.com>
2020-01-09 14:51:32 +0900

    memo

--- ch03/variable.exs
+++ ch03/variable.exs
@@ -1,3 +1,11 @@
 n = 1
 n = n + 1
 IO.puts(n)
+
+IO.puts("AAA")
+IO.puts(~s(AAA #{n} \t A))
+IO.puts(~S(AAA #{n} \t A))
+
+IO.puts(:hoge)
+
+IO.puts(:rand.uniform(100))
---
+++ memo.md
@@ -0,0 +1,11 @@
+# MEMO
+
+## Type Validation
+
+- is_integer
+- is_binary
+- String.valid? --- UTF8 の文字列かどうかを調べる。ガード節では使えない
+
+## IEx
+
+- `i(target)`関数で色々と情報を表示できる
