8943232a4c7506b77a90348fabe5eca902d08aae
aliyome <aliyome@gmail.com>
2020-01-11 17:13:41 +0900

    format duration

--- nano_planner/lib/nano_planner_web/views/plan_items_view.ex
+++ nano_planner/lib/nano_planner_web/views/plan_items_view.ex
@@ -1,3 +1,19 @@
 defmodule NanoPlannerWeb.PlanItemsView do
   use NanoPlannerWeb, :view
+  alias Timex.Format.DateTime.Formatters.Strftime
+
+  def format_duration(item) do
+    s = item.starts_at |> utc_to_jst()
+    e = item.ends_at |> utc_to_jst()
+    "#{format_utc(s)} - #{format_utc(e)}"
+  end
+
+  defp format_utc(utc) do
+    Strftime.format!(utc, "%Y/%m/%d %H:%M")
+  end
+
+  defp utc_to_jst(utc) do
+    tokyo_tz = Timex.Timezone.get("Asia/Tokyo")
+    utc |> Timex.Timezone.convert(tokyo_tz)
+  end
 end
