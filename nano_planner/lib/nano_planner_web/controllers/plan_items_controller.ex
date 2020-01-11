7c2ed94efa061eef596a022edf994aa9e3d8e92a
aliyome <aliyome@gmail.com>
2020-01-11 14:58:29 +0900

    alias for shorthand

--- nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
+++ nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
@@ -1,8 +1,10 @@
 defmodule NanoPlannerWeb.PlanItemsController do
   use NanoPlannerWeb, :controller
+  alias NanoPlanner.Repo
+  alias NanoPlanner.PlanItem
 
   def index(conn, _params) do
-    plan_items = NanoPlanner.Repo.all(NanoPlanner.PlanItem)
+    plan_items = Repo.all(PlanItem)
     render(conn, "index.html", plan_items: plan_items)
   end
 end
