55d986a6951ab39791f4b4f3952da8eaa6a18a95
aliyome <aliyome@gmail.com>
2020-01-11 16:16:40 +0900

    sort by starts_at

--- nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
+++ nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
@@ -2,9 +2,10 @@ defmodule NanoPlannerWeb.PlanItemsController do
   use NanoPlannerWeb, :controller
   alias NanoPlanner.Repo
   alias NanoPlanner.PlanItem
+  import Ecto.Query
 
   def index(conn, _params) do
-    plan_items = Repo.all(PlanItem)
+    plan_items = PlanItem |> order_by(:starts_at) |> Repo.all()
     render(conn, "index.html", plan_items: plan_items)
   end
 end
