6d1d2521804cd508ecb024c3a4710cc351a94cbd
aliyome <aliyome@gmail.com>
2020-01-11 14:15:13 +0900

    seed

--- nano_planner/priv/repo/seeds.exs
+++ nano_planner/priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias NanoPlanner.Repo, as: Repo
+alias NanoPlanner.PlanItem, as: PlanItem
+
+Repo.insert!(%PlanItem{
+  name: "読書",
+  description: "「走れメロス」を読む"
+})
